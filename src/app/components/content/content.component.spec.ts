4decb2d43ad39d4675d62ba4fdfcfb7f86fdfe66
André Hopf <andre.hopf@gmail.com>
2023-05-14 18:19:21 -0300

    Exercício 2

---
+++ src/app/components/content/content.component.spec.ts
@@ -0,0 +1,25 @@
+import { ContentComponent } from "./content.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+
+describe('ContentComponent', () => {
+	let component: ContentComponent;
+	let fixture: ComponentFixture<ContentComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ContentComponent],
+			imports: [HttpClientModule],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA]
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(ContentComponent);
+		component = fixture.componentInstance;
+	});
+
+	it('Should create component', () => {
+		expect(component).toBeTruthy();
+	});
+
+})
